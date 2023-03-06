63a7b301e42f0e61d3e1c27ea2cf6849a11c8d39
lesayobruce <lesayobruce419@gmail.com>
2023-03-06 14:44:51 +0300

    compiled codes for home file

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Home() {
+  return (
+    <div className="container">
+      <h1>Welcome to Meme Republica</h1>
+      <span><p>Meme Republic is a one stop app to transform you into a MEME LORD.</p></span>
+      <span><p>To get started, please <a href="/register">register</a> or <a href="/login">log in</a>.</p></span>
+    </div>
+  );
+}
+export default Home;
