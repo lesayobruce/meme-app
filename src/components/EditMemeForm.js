f2e922ce1817b87dad461a9cc792bd03d166ffd4
lesayobruce <lesayobruce419@gmail.com>
2023-03-06 14:47:02 +0300

    compiled codes for editing memes file

---
+++ src/components/EditMemeForm.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+const EditMemeForm = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchMeme = async () => {
+      setLoading(true);
+      const response = await axios.get(`http://localhost:5000/memes/${id}`);
+      const meme = response.data;
+      setTitle(meme.title);
+      setDescription(meme.description);
+      setImageUrl(meme.imageUrl);
+      setLoading(false);
+    };
+    fetchMeme();
+  }, [id]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const updatedMeme = { title, description, imageUrl };
+    await axios.put(`http://localhost:5000/memes/${id}`, updatedMeme);
+    setLoading(false);
+    navigate('/my-memes');
+  };
+  return (
+    <div>
+      <h2>Edit Meme</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description</label>
+            <input
+              type="text"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="imageUrl">Image URL</label>
+            <input
+              type="text"
+              id="imageUrl"
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+            />
+          </div>
+          <button type="submit">Save Changes</button>
+        </form>
+      )}
+    </div>
+  );
+};
+export default EditMemeForm;
